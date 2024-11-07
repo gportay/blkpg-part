90326eeacc9ac5ff4568d8845397c7226e23fe6d
Gaël PORTAY <gael.portay@rtone.fr>
2024-11-07 02:36:18 +0100

    blkpg-part.{hash,mk}: set version to v1
    Fixes:
    
    	$ make part-legal-info
    	(...)
    	ERROR: No hash found for blkpg-part-master.tar.gz

--- support/blkpg-part.mk
+++ support/blkpg-part.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BLKPG_PART_VERSION = master
+BLKPG_PART_VERSION = 5a4ec5f53ed904b37fba03f3797fbe2af3077f8d
 BLKPG_PART_SITE = $(call github,gportay,blkpg-part,$(BLKPG_PART_VERSION))
 BLKPG_PART_LICENSE = LGPL-2.1-or-later
 BLKPG_PART_LICENSE_FILES = LICENSE
